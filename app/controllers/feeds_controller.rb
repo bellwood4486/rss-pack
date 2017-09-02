b872eaab8161508f6a422b5cb9cc57e3f27d96f2
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-09-03 00:08:09 +0900

    Add new page.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,4 +2,23 @@ class FeedsController < ApplicationController
   def index
     @feeds = Feed.all
   end
+
+  def new
+    @feed = Feed.new
+  end
+
+  def create
+    @feed = Feed.new(feed_create_params)
+    if @feed.save
+      redirect_to feeds_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def feed_create_params
+    params.require(:feed).permit(:url)
+  end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,2 +1,4 @@
 class Feed < ApplicationRecord
+  validates :url, presence: true
+  validates :title, presence: true
 end
