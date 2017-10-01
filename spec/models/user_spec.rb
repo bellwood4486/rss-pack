81df63455b0e9c56403be3a244f81e9af2182491
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-10-01 18:45:13 +0900

    Replace setup logic of the user spec with FactoryGirl.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,47 +5,35 @@ describe User, type: :model do
     expect(FactoryGirl.build(:user)).to be_valid
   end
 
-  it 'メールアドレス、パスワード(確認用も)があれば有効な状態であること' do
-    user = User.new(email: 'test@example.com',
-                    password: 'secret',
-                    password_confirmation: 'secret')
-    expect(user).to be_valid
-  end
-
   it 'メールアドレスがなければ無効な状態であること' do
-    user = User.new(email: nil)
+    user = FactoryGirl.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include "can't be blank"
   end
 
   it 'パスワードがなければ無効な状態であること' do
-    user = User.new(password: nil)
+    user = FactoryGirl.build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include "can't be blank"
   end
 
   it 'パスワードと確認用パスワードが一致しなければ不正な状態であること' do
-    user = User.new(password: 'secret',
-                    password_confirmation: '12345')
+    user = FactoryGirl.build(:user,
+                             password: 'secret',
+                             password_confirmation: '12345')
     user.valid?
     expect(user.errors[:password_confirmation]).to include "doesn't match Password"
   end
 
   it '重複したメールアドレスは無効な状態であること' do
-    User.create(email: 'test@example.com',
-                password: 'secret',
-                password_confirmation: 'secret')
-    user = User.new(email: 'test@example.com',
-                    password: 'secret',
-                    password_confirmation: 'secret')
+    FactoryGirl.create(:user, email: 'test@example.com')
+    user = FactoryGirl.build(:user, email: 'test@example.com')
     user.valid?
     expect(user.errors[:email]).to include "has already been taken"
   end
 
   it '保存されるメールアドレスは小文字になっていること' do
-    user = User.create(email: 'TEST@EXAMPLE.COM',
-                password: 'secret',
-                password_confirmation: 'secret')
+    user = FactoryGirl.create(:user, email: 'TEST@EXAMPLE.COM')
     user.reload
     expect(user.email).to eq 'test@example.com'
   end
