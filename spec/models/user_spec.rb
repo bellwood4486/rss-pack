9d423f1a2b30dd93d7680697a8d618dce01a1e2d
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-10-01 18:53:20 +0900

    Simplify FactoryGirl syntax.
    Include FactoryGirl::Sytax::Methods.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,38 +2,38 @@ require 'rails_helper'
 
 describe User, type: :model do
   it '有効なファクトリを持つこと' do
-    expect(FactoryGirl.build(:user)).to be_valid
+    expect(build(:user)).to be_valid
   end
 
   it 'メールアドレスがなければ無効な状態であること' do
-    user = FactoryGirl.build(:user, email: nil)
+    user = build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include "can't be blank"
   end
 
   it 'パスワードがなければ無効な状態であること' do
-    user = FactoryGirl.build(:user, password: nil)
+    user = build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include "can't be blank"
   end
 
   it 'パスワードと確認用パスワードが一致しなければ不正な状態であること' do
-    user = FactoryGirl.build(:user,
-                             password: 'secret',
-                             password_confirmation: '12345')
+    user = build(:user,
+                 password: 'secret',
+                 password_confirmation: '12345')
     user.valid?
     expect(user.errors[:password_confirmation]).to include "doesn't match Password"
   end
 
   it '重複したメールアドレスは無効な状態であること' do
-    FactoryGirl.create(:user, email: 'test@example.com')
-    user = FactoryGirl.build(:user, email: 'test@example.com')
+    create(:user, email: 'test@example.com')
+    user = build(:user, email: 'test@example.com')
     user.valid?
     expect(user.errors[:email]).to include "has already been taken"
   end
 
   it '保存されるメールアドレスは小文字になっていること' do
-    user = FactoryGirl.create(:user, email: 'TEST@EXAMPLE.COM')
+    user = create(:user, email: 'TEST@EXAMPLE.COM')
     user.reload
     expect(user.email).to eq 'test@example.com'
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -54,4 +54,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # ファクトリを簡単に呼び出せるよう、Factory Girlの構文をインクルードする
+  config.include FactoryGirl::Syntax::Methods
 end
