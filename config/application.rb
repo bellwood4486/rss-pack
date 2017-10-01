6ccbeb085d57fbec5c7e4ddc30a0aaef42fc13ab
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-10-01 16:05:17 +0900

    Add configurations for rspec.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module RssPack
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_spec: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_girl,
+                            dir: 'spec/factories'
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
