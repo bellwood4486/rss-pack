97737db4ccfbd18a6f166f1a065b56fe5eaba54f
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-10-08 14:42:55 +0900

    Add examples for UsersController.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+describe UsersController, type: :controller do
 
+  describe 'GET #new' do
+    it '@contract に新しい連絡先を割り当てること' do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+
+    it ':new テンプレートを表示すること' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context '有効な属性の場合' do
+      it 'データベースに新しいユーザーを保存すること' do
+        expect {
+          post :create, params: { user: attributes_for(:user) }
+        }.to change(User, :count).by(1)
+      end
+      it 'トップページにリダイレクトすること' do
+        post :create, params: { user: attributes_for(:user) }
+        expect(response).to redirect_to root_url
+      end
+      it '新しいユーザーはPackを1つ持っていること'
+    end
+
+    context '無効な属性の場合' do
+      it 'データベースに新しいユーザーを保存しないこと' do
+        expect {
+          post :create, params: { user: attributes_for(:invalid_user) }
+        }.not_to change(User, :count)
+      end
+
+      it ':new テンプレートを再表示すること' do
+        post :create, params: { user: attributes_for(:invalid_user) }
+        expect(response).to render_template :new
+      end
+    end
+
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password 'secret'
     password_confirmation 'secret'
+
+    factory :invalid_user do
+      email nil
+    end
   end
 end
