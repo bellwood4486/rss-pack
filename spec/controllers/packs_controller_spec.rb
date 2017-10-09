081486b3abf69e3aad80c91620d78d8ed1fb3e0b
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-10-09 17:25:17 +0900

    Refactor pack factories.

--- spec/controllers/packs_controller_spec.rb
+++ spec/controllers/packs_controller_spec.rb
@@ -3,17 +3,16 @@ require 'rails_helper'
 describe PacksController, type: :controller do
   describe 'GET #rss' do
     context '存在するrss_tokenの場合' do
-      let(:pack) { build_stubbed(:pack, rss_token: 'dummytoken') }
+      let(:pack) { build_stubbed(:pack_with_rss_content) }
 
       before :each do
         allow(Pack).to receive(:find_by).and_return(pack)
         allow(pack).to receive(:refresh!)
-        allow(pack).to receive(:rss_content).and_return('dummycontent')
       end
 
       it 'rss_tokenに対応するrss_contentを返すこと' do
-        get :rss, params: { token: 'dummytoken' }
-        expect(response.body).to eq 'dummycontent'
+        get :rss, params: { token: pack.rss_token }
+        expect(response.body).to eq pack.rss_content
       end
     end
 
