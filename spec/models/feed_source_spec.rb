bb2482171ea123cd75827a93ff18308baa50a6bb
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-10-09 16:15:24 +0900

    Add feed source examples.

--- spec/models/feed_source_spec.rb
+++ spec/models/feed_source_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe FeedSource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe FeedSource, type: :model do
+  it '有効なファクトリを持つこと' do
+    expect(build(:feed_source)).to be_valid
+  end
+
+  it 'URLがなければ無効な状態であること' do
+    source = build(:feed_source, url: nil)
+    source.valid?
+    expect(source.errors[:url]).to include "can't be blank"
+  end
 end
