cf93544a057d0aa74b4d1f04641db27b2e8cd3d3
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2017-10-13 23:55:08 +0900

    Modify user info area.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_for(user, size: 80, class_attr: '')
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.email, class: "#{class_attr}")
+  end
 end
