246df9941e97be602d7988e599c60c9af741233b
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2019-05-06 00:35:04 +0900

    refactor: テストコードをリファクタリング

--- spec/factories/packs.rb
+++ spec/factories/packs.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :pack do
     user
     sequence(:name) {|n| "パック#{n}" }
+    sequence(:rss_content) {|n| "RSSコンテンツ#{n}" }
+    rss_created_at { Faker::Date.between(1.year.ago, Time.current) }
 
     trait :with_subscription do
       after(:create) do |pack|
--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :subscription do
     pack
     feed
+    sequence(:message) {|n| "メッセージ#{n}" }
+    read_timestamp { Faker::Date.between(1.year.ago, Time.current) }
+    messaged_at { Faker::Date.between(1.year.ago, Time.current) }
   end
 end
