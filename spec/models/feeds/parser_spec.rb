d6aee7348846a967d2f5aef702f808679f28b4be
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2019-05-05 22:15:54 +0900

    test: parser_specの不要な記述を削除

--- spec/models/feeds/parser_spec.rb
+++ spec/models/feeds/parser_spec.rb
@@ -1,8 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Feeds::Parser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
   describe "parse_content!" do
     let(:parser) { Feeds::Parser.new }
 
